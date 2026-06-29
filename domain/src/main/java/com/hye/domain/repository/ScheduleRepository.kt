37638b87f1bcb99135f44d32363457e180b48516
deoya <deoya@naver.com>
2026-06-29 23:23:36 +0900

    [feat]Repository 및 UseCase 구현

--- domain/src/main/java/com/hye/domain/repository/ScheduleRepository.kt
+++ domain/src/main/java/com/hye/domain/repository/ScheduleRepository.kt
@@ -1,4 +1,10 @@
 package com.hye.domain.repository
 
+import com.hye.domain.model.WorkSchedule
+import kotlinx.coroutines.flow.Flow
+
 interface ScheduleRepository {
+    fun getSchedule(): Flow<WorkSchedule>
+
+    suspend fun saveSchedule(schedule: WorkSchedule)
 }
