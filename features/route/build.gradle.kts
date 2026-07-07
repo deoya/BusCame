2172d3c7034fecdab3e078d5ac8b4496bd0e0e91
deoya <deoya@naver.com>
2026-07-07 21:19:29 +0900

    [chore]route 모듈 생성 및 환경 세팅

---
+++ features/route/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.hilt.gradle)
+}
+
+android {
+    namespace = "com.hye.features.route"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":common:design"))
+    implementation(project(":domain"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.android.core)
+    implementation(libs.bundles.compose)
+
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.timber)
+}
